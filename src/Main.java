afb2f7c87f54d0cc369c231cd306e4b97b45511b
Flawden <topchiidv1998@gmail.com>
2023-12-13 20:40:44 +0300

    Задания №2 и №3

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,15 @@ public class Main {
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
-        System.out.println("Значение dog = " + dog + ", значение cat = " + cat + ", значение paper = " + paper);
+        System.out.println("Значение dog = " + dog + ", значение cat = " + cat + ", а значение paper = " + paper + ".");
+        dog+=4;
+        cat+=4;
+        paper+=4;
+        System.out.println("Значение dog теперь = " + dog + ", значение cat = " + cat + ", а значение paper = " + paper + ".");
+        dog-=3.5;
+        cat-=1.6;
+        paper-=7639;
+        System.out.println("Значение dog теперь = " + dog + ", значение cat = " + cat + ", а значение paper = " + paper + ".");
+
     }
 }
