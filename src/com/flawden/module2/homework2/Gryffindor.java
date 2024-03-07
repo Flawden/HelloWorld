7238f97360b93bda5615741bb59496c50e8d03d8
Flawden <topchiidv1998@gmail.com>
2024-03-07 14:06:43 +0300

    gryffindor students can now be compared.

--- src/com/flawden/module2/homework2/Gryffindor.java
+++ src/com/flawden/module2/homework2/Gryffindor.java
@@ -48,4 +48,49 @@ public class Gryffindor extends HogwartsStudent {
                 "Witchcraft: " + witchcraft + "\n" +
                 "The distance of transgression: " + transgressionDistance + "\n";
     }
+
+    public static void compareStudentsByNobility(Gryffindor studentOne, Gryffindor studentTwo) {
+        System.out.println("The following students took part in the nobility comparison:");
+        System.out.println(studentOne.getFullName() + " with an indicator of nobility: " + studentOne.getNobility());
+        System.out.println(studentTwo.getFullName() + " with an indicator of nobility: " + studentTwo.getNobility());
+        System.out.print("Winner: ");
+        if (studentOne.getNobility() == studentTwo.getNobility()) {
+            System.out.println("Nobody. Students are equal");
+        }
+        else if (studentOne.getNobility() > studentTwo.getNobility()) {
+            System.out.println(studentOne);
+        } else {
+            System.out.println(studentTwo);
+        }
+    }
+
+    public static void compareStudentsByHonor(Gryffindor studentOne, Gryffindor studentTwo) {
+        System.out.println("The following students took part in the honor comparison:");
+        System.out.println(studentOne.getFullName() + " with an indicator of honor: " + studentOne.getHonor());
+        System.out.println(studentTwo.getFullName() + " with an indicator of honor: " + studentTwo.getHonor());
+        System.out.print("Winner: ");
+        if (studentOne.getHonor() == studentTwo.getHonor()) {
+            System.out.println("Nobody. Students are equal");
+        }
+        else if (studentOne.getHonor() > studentTwo.getHonor()) {
+            System.out.println(studentOne);
+        } else {
+            System.out.println(studentTwo);
+        }
+    }
+
+    public static void compareStudentsByBravery(Gryffindor studentOne, Gryffindor studentTwo) {
+        System.out.println("The following students took part in the bravery comparison:");
+        System.out.println(studentOne.getFullName() + " with an indicator of bravery: " + studentOne.getBravery());
+        System.out.println(studentTwo.getFullName() + " with an indicator of bravery: " + studentTwo.getBravery());
+        System.out.print("Winner: ");
+        if (studentOne.getBravery() == studentTwo.getBravery()) {
+            System.out.println("Nobody. Students are equal");
+        }
+        else if (studentOne.getBravery() > studentTwo.getBravery()) {
+            System.out.println(studentOne);
+        } else {
+            System.out.println(studentTwo);
+        }
+    }
 }
